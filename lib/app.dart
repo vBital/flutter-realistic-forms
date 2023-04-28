51637219aedb20153bd9e2f7fd692accd8899d03
Diogo C. Pereira <130650392+vBital@users.noreply.github.com>
2023-04-28 10:07:45 +0100

    Add files via upload

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'screens/home_material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Forms Demo',
+      home: HomeMaterial(),
+    );
+  }
+}
